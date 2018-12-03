cc145afb77c275bddc1ed6a8dfe9f727e107ab81
Mary Dickson Diaz <mdickson@navigatingcancer.com>
2018-12-02 17:13:51 -0800

    day 2 part 1

--- lib/2.rb
+++ lib/2.rb
@@ -1,4 +1,18 @@
-def part_1_2(input)
+def part_1_2(inputs)
+  twos = []
+  threes = []
+  inputs.each do |input|
+    input.each_char do |char|
+      if input.count(char) == 3
+        threes << input unless threes.include?(input)
+      end
+      if input.count(char) == 2
+        twos << input unless twos.include?(input)
+        next
+      end
+    end
+  end
+  twos.count * threes.count
 end
 
 def part_2_2(input)
--- spec/2_spec.rb
+++ spec/2_spec.rb
@@ -1,17 +1,17 @@
-require "spec_helper"
-require "2.rb"
+require 'spec_helper'
+require '2.rb'
 
-describe "2" do
-  test_input = []
-  input = IO.read("inputs/2.txt").split("\n")
+describe '2' do
+  test_input = ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']
+  input = IO.read('inputs/2.txt').split("\n")
 
-  it "returns accurate test data" do
-    assert_equal nil, part_1_2(test_input)
-    assert_equal nil, part_2_2(test_input)
+  it 'returns accurate test data' do
+    assert_equal 12, part_1_2(test_input)
+    # assert_equal part_2_2(test_input)
   end
 
-  it "returns a valid result" do
-    assert_equal nil, part_1_2(input)
-    assert_equal nil, part_2_2(input)
+  it 'returns a valid result' do
+    assert_equal 7688, part_1_2(input)
+    # assert_nil part_2_2(input)
   end
 end
