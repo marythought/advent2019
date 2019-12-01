a1637a64b40cf00e19529af0a13657302d8ddcec
Mary Dickson Diaz <mdickson@navigatingcancer.com>
2019-12-01 14:02:32 -0800

    init commit

--- spec/1_spec.rb
+++ spec/1_spec.rb
@@ -1,19 +1,17 @@
 require "spec_helper"
 require "1.rb"
 
-describe '1' do
-  test_input = [1, -2, 3, 1]
-  input = IO.read("inputs/1.txt").split("\n").map(&:to_i)
+describe "1" do
+  test_input = []
+  input = IO.read("inputs/1.txt").split("\n")
 
-  it "returns a valid result for part 1" do
-    assert_equal 3, Day1.frequency(test_input)
-    assert_equal 531, Day1.frequency(input)
+  it "returns accurate test data" do
+    assert_nil part_1_1(test_input)
+    assert_nil part_2_1(test_input)
   end
 
-  it "returns valid response for part 2" do
-    a = Day1.new
-    assert_equal 2, a.twice_frequency(test_input)
-    b = Day1.new
-    assert_equal 76_787, b.twice_frequency(input)
+  it "returns a valid result" do
+    assert_nil part_1_1(input)
+    assert_nil part_2_1(input)
   end
 end
